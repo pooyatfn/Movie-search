cfeb2df8b9aa36c42ed587612ef6bbde2fb18b6c
pooya <@toofanian>
2024-04-19 19:05:51 +0330

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12-slim-bullseye
+
+WORKDIR /app
+
+COPY ./requirements.txt ./requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
+
+COPY endpoint.py .
+COPY templates templates
+
+CMD ["uvicorn", "endpoint:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ endpoint.py
@@ -0,0 +1,62 @@
+import json
+
+import redis
+import requests
+import uvicorn
+from elasticsearch import Elasticsearch
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+redis_client = redis.Redis(host='redis', port=6379, password="SomePass@")
+
+es = Elasticsearch("http://elasticsearch:9200", http_auth=('elastic', 'SomePass@'), verify_certs=False)
+
+API_URL = "https://imdb146.p.rapidapi.com/v1/find/"
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def search_form(request: Request):
+    return templates.TemplateResponse("search_form.html", {"request": request})
+
+
+@app.get("/search/", response_class=HTMLResponse)
+async def search_movie(request: Request, movie_name: str):
+    search_result = redis_client.get(movie_name)
+    if search_result:
+        movies_result = [item for item in json.loads(search_result)]
+        return templates.TemplateResponse("movie_details.html", {"request": request, "movies_result": movies_result})
+
+    es_response = es.search(index='movies', body={"query": {"match": {"Series_Title": movie_name}}})
+    if es_response['hits']['total']['value'] > 0:
+        search_result = es_response['hits']['hits']
+        movies_result = [item['_source'] for item in search_result]
+        redis_client.set(movie_name, json.dumps(movies_result))
+        return templates.TemplateResponse("movie_details.html", {"request": request, "movies_result": movies_result})
+
+    querystring = {"query": movie_name}
+    headers = {
+        "X-RapidAPI-Key": "cd43af9cfdmsh6c224a18db2e7d9p182ab9jsneb1b946a7f1b",
+        "X-RapidAPI-Host": "imdb146.p.rapidapi.com"
+    }
+    api_response = requests.get(API_URL, headers=headers, params=querystring,
+                                proxies={"https": "socks5://proxy.my.sahab.ir:8123",
+                                         "http": "socks5://proxy.my.sahab.ir:8123"})
+    if api_response.status_code == 200:
+        search_result = api_response.json()['titleResults']['results']
+        if len(search_result) == 0:
+            return templates.TemplateResponse("no_movies.html", {"request": request})
+        movies_result = [{'Poster_Link': item['titlePosterImageModel']['url'], 'Series_Title': item['titleNameText'],
+                          'Released_Year': item['titleReleaseText'], 'Stars': item['topCredits']} for item in
+                         search_result]
+        return templates.TemplateResponse("movie_details.html", {"request": request, "movies_result": movies_result})
+
+    return templates.TemplateResponse("no_movies.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
